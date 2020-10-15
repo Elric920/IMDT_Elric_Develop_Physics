9cdff997002b91cd1a3b8029677c384f99240bbd
Elric920 <1871206768@qq.com>
2020-10-15 19:43:24 +0800

    第一次提交

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BallController : MonoBehaviour
+{
+    public float forcePower = 1.0f;
+    // Start is called before the first frame update
+    //void Start()
+    //{
+        
+    //}
+
+    // Update is called once per frame
+    void Update()
+    {
+        float posX = Input.GetAxis("Horizontal");
+        float posY = Input.GetAxis("Vertical");
+        if (posX>0)
+        {
+            gameObject.GetComponent<Rigidbody>().velocity = Vector3.right * forcePower;
+        }
+        else if (posX<0)
+        {
+            gameObject.GetComponent<Rigidbody>().velocity = Vector3.left * forcePower;
+        }
+        if (posY > 0)
+        {
+            gameObject.GetComponent<Rigidbody>().velocity = Vector3.forward * forcePower;
+        }
+        else if (posY < 0)
+        {
+            gameObject.GetComponent<Rigidbody>().velocity = Vector3.back * forcePower;
+        }
+    }
+}
